a978456c3c37bfca32fb21152e08f5cba037d964
Eric Noubissi <noubissi10@yahoo.fr>
2022-02-23 12:55:42 +0100

    Création d'un API

---
+++ routes/postsController.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const router = express.Router();
+const ObjectID = require('mongoose').Types.ObjectId;
+
+const { PostsModel } = require('../models/postsModel');
+
+// AFFICHER LES DATAS
+router.get('/', (req, res) => {
+  PostsModel.find((err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("Error to get data : " + err);
+  })
+});
+
+// CRÉER DES DATAS
+router.post('/', (req, res) => {
+    const newRecord = new PostsModel({
+      author: req.body.author,
+      message: req.body.message
+    });
+
+    newRecord.save((err, docs) => {
+        if (!err) res.send(docs);
+        else console.log('Error creating new data : ' + err);
+    })
+});
+
+// MODIFIER LES DATAS
+router.put("/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send("ID unknow : " + req.params.id)
+    
+    const updateRecord = {
+      author: req.body.author,
+      message: req.body.message
+    };
+
+    PostsModel.findByIdAndUpdate(
+        req.params.id,
+        { $set: updateRecord},
+        { new: true },
+        (err, docs) => {
+          if (!err) res.send(docs);
+          else console.log("Update error : " + err);
+        }
+    )
+
+});
+
+// DELETE DATA
+router.delete("/:id", (req, res) => {
+    if (!ObjectID.isValid(req.params.id))
+      return res.status(400).send("ID unknow : " + req.params.id)
+    
+    PostsModel.findByIdAndRemove(
+      req.params.id,
+      (err, docs) => {
+        if (!err) res.send(docs);
+        else console.log("Delete error : " + err);
+    })
+});
+
+module.exports = router;
